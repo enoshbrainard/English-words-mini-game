33eb3d31f5a624667580cc54c42c6ef08449271f
enoshbrainard <enoshbrainard8@gmail.com>
2025-08-07 12:21:07 +0530

    change vit.config ,added .png

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt"],
+      manifest: {
+        name: "Words Game",
+        short_name: "Words",
+        description: "Guess the word game you can play offline!",
+
+        start_url: "/",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#1d4ed8", // Tailwind blue-700
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
